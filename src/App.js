4fb0ac92f954973ae8a8d853de4516677a1aff4c
Duduhiro <90015798+Duduhiro@users.noreply.github.com>
2023-10-08 19:48:40 -0300

    First few changes on the project. Added search bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import './reset.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Search/>
+    </div>
+  );
+}
+
+function Search(props) {
+  return(
+    <div className="search-div">
+      <div>
+        <h1>Search a hero</h1>
+        <input />
+      </div>
+      <button>Find</button>   
     </div>
   );
 }
